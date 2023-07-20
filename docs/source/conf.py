ae410fbeab8efd7fe241bf8226257a0ae89204a8
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-07-20 18:14:32 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PSProcessing'
+copyright = '2023, Shadi'
+author = 'Shadi'
 import os
 release = '0.1'
 version = '0.1.0'
