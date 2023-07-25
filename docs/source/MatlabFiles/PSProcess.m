9bc8b1c29606aa3397ff8f817ff4c26b3dbe378f
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-07-25 12:03:55 -0400

    Update PSProcess.m

--- docs/source/MatlabFiles/PSProcess.m
+++ docs/source/MatlabFiles/PSProcess.m
@@ -1,4 +1,5 @@
 function out = PSProcess(S1,S2,procStruct)
+%PSProcess - computes local birefringence
 
     %PSProcess computes local birefringence of the 
     %Stokes vectors S1 and S2, corresponding to the Stokes vectors measured 
